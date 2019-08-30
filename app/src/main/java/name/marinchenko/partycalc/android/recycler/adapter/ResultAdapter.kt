aa9e8a11e5411684971e069d8621c07e28050a47
Marinchenko Valentin <marinchenkovalentin@gmail.com>
2019-08-30 17:24:48 +0300

    ru names and titles

--- app/src/main/java/name/marinchenko/partycalc/android/recycler/adapter/ResultAdapter.kt
+++ app/src/main/java/name/marinchenko/partycalc/android/recycler/adapter/ResultAdapter.kt
@@ -36,10 +36,6 @@ class ResultAdapter(private val ctx: Context) : RecyclerView.Adapter<ResultViewH
         onLoad?.invoke()
     }
 
-    fun checkDone() {
-
-    }
-
     override fun getItemCount() = list.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
